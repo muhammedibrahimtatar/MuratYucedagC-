6e6465fb3b9e3179486bb892f7f295ded8a3c405
M. Ibrahim TATAR <muhammedibrahimtatar@hotmail.com>
2024-10-05 20:52:15 +0300

    Add project files.

---
+++ 01-MainSubjects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Yazdırma Komutları
+            //Console.WriteLine("WriteLine:Yazı yazıp bir alt satıra iner");
+            //Console.WriteLine("Write:Yazı yazar ancak alt satıra inmez");
+            #endregion
+
+            #region String Degisken
+            ////Metinsel ifade değişkeni
+            //string customerName;
+            //string customerSurname;
+
+            //customerName = "Ali";
+            //customerSurname = "Çınar";
+
+            //Console.WriteLine(customerName + " " + customerSurname);
+            #endregion
+
+            #region Integer Degisken
+            ////Tamsayı ifade değişkeni
+            //int price = 2;
+            //int count = 3;
+
+            //Console.WriteLine($"Price:"+price + " Count:"+ + count);
+            #endregion
+
+
+
+            Console.Read();
+        }
+    }
+}
