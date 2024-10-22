cf4dc3cada0e8c80f4d6ce9e3c2f635f4e381dc5
M. Ibrahim TATAR <muhammedibrahimtatar@hotmail.com>
2024-10-22 21:31:05 +0300

    9.Ders sonu

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ado.net
+
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+            string tableNumber;
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.WriteLine("------------------------");
+            Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+            tableNumber = Console.ReadLine();
+
+
+            SqlConnection connection = new SqlConnection("Data Source=IBRAHIMTATAR\\MSSQLSERVER01;Initial Catalog=EgitimKampiDb;Integrated Security=True;"
+);
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * From TblCategory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable datatable = new DataTable();
+            adapter.Fill(datatable);
+            connection.Close();
+
+            foreach (DataRow row in datatable.Rows)
+            {
+                foreach(var item in row.ItemArray)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+            Console.Read();
+        }
+    }
+}
